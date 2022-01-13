e86a5b08c4e4f68dab2a76361af0701e7b9775ea
pedro.molina <pedro.molina@petroprix.com>
2022-01-13 12:15:19 +0100

    shus

--- lib/src/widget/shoes_size_preview.dart
+++ lib/src/widget/shoes_size_preview.dart
@@ -15,7 +15,8 @@ class ShoeSizePreview extends StatelessWidget {
         child: Column(
           children: [
             //zapato con sombra
-            _ShadowShoe()
+            _ShadowShoe(),
+            _SizeShoe()
             //TODO: tallas
           ],
         ),
@@ -27,10 +28,82 @@ class ShoeSizePreview extends StatelessWidget {
 class _ShadowShoe extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Positioned(bottom: 20, right: 50, child: _Shadow()),
+        Padding(
+          padding: EdgeInsets.all(50.0),
+          child: Image(image: AssetImage("assets/imgs/azul.png")),
+        ),
+      ],
+    );
+  }
+}
+
+class _Shadow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Transform.rotate(
+      angle: -0.5,
+      child: Container(
+        width: 230,
+        height: 120,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(100),
+            boxShadow: [BoxShadow(color: Color(0xffEAA14E), blurRadius: 40)]),
+      ),
+    );
+  }
+}
+
+class _SizeShoe extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(50.0),
-      child: Stack(
-        children: [Image(image: AssetImage("assets/imgs/azul.png"))],
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          _SizeShoeContainer(7),
+          _SizeShoeContainer(7.5),
+          _SizeShoeContainer(8),
+          _SizeShoeContainer(8.5),
+          _SizeShoeContainer(9),
+          _SizeShoeContainer(9.5),
+        ],
+      ),
+    );
+  }
+}
+
+class _SizeShoeContainer extends StatelessWidget {
+  final double number;
+  _SizeShoeContainer(this.number);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 45,
+      alignment: Alignment.center,
+      child: Text(
+        number.toString().replaceAll(".", ","),
+        style: TextStyle(
+            color: number == 9 ? Colors.white : Color(0xffFFcF53),
+            fontSize: 16,
+            fontWeight: FontWeight.bold),
+      ),
+      height: 45,
+      decoration: BoxDecoration(
+        color: number == 9 ? Color(0xffF1A23A) : Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          if (number == 9)
+            BoxShadow(
+                offset: Offset(0.0, 5.0),
+                color: Color(0xffF1A23A).withOpacity(0.2),
+                spreadRadius: 5,
+                blurRadius: 2),
+        ],
       ),
     );
   }
